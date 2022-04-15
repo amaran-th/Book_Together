d00bf59bc8f5dcc39d80f9d98c823f5f5d1eb439
amaran-th <songsy405@gmail.com>
2022-04-16 06:01:33 +0900

    feat:메인페이지 사이드바 간격 조정 #2

--- src/page/Main/Main.jsx
+++ src/page/Main/Main.jsx
@@ -9,16 +9,16 @@ import Option from './Side/Option'
 function Main() {
   return (
     <div className="dark:bg-black dark:text-white">
-      <div className="border border-black max-w-3xl mx-auto sm:px-6 md:max-w-7xl md:px-8 md:grid md:grid-cols-12 md:gap-8">
-        <div className="border border-black hidden md:block md:col-span-4 lg:col-span-3">
-          <nav name="좌측 사이드바" className="sticky top-4">
+      <div className="max-w-3xl mx-auto sm:px-6 md:max-w-7xl md:px-8 md:grid md:grid-cols-12 md:gap-8">
+        <div className="hidden md:block md:col-span-4 lg:col-span-3">
+          <nav name="좌측 사이드바" className="sticky top-4 space-y-4 mt-4">
             <Friend />
             <Club />
           </nav>
         </div>
         <Content />
         <aside className="hidden xl:block xl:col-span-3">
-          <section name="우측 사이드바" className="sticky top-4 space-y-4">
+          <section name="우측 사이드바" className="sticky top-4 space-y-4 mt-4">
             <Option />
           </section>
         </aside>
